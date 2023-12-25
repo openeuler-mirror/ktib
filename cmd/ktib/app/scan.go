527567ab448fb07149ee49d32065a948cd15fa80
huayumeng <huayumeng@kylinos.cn>
2023-12-25 10:54:59 +0800

    Fix ktib scan panic, finish InitScanOptions

--- cmd/ktib/app/scan.go
+++ cmd/ktib/app/scan.go
@@ -13,26 +13,23 @@ package app
 
 import (
 	"context"
-	"fmt"
 	o "gitee.com/openeuler/ktib/cmd/ktib/app/options"
 	"gitee.com/openeuler/ktib/pkg/types"
 	"github.com/aquasecurity/trivy/pkg/commands/artifact"
 	"github.com/spf13/cobra"
 	"github.com/urfave/cli/v2"
+	"os"
+	"path/filepath"
 )
 
 func runScan(c *cobra.Command, args []string, opt o.Option) error {
 	//TODO 解析context 构造scanner  in pkg/scanner
-	var ctx cli.Context
-	option, err := artifact.InitOption(&ctx)
-	if err != nil {
-		return err
-	}
 	//TODO: 需要对比ktib option和trivy option的区别，参数不足需要额外赋值
-	opt.Option = option
-	// 1111111111
-	fmt.Println(option)
-	runner, err := artifact.NewRunner(opt.Option)
+	var ctx cli.Context
+	ctx.Context = context.Background()
+	ctx.App = cli.NewApp()
+	scanOption, err := o.InitScanOptions(opt, ctx)
+	runner, err := artifact.NewRunner(scanOption)
 	if err != nil {
 		return err
 	}
@@ -42,7 +39,7 @@ func runScan(c *cobra.Command, args []string, opt o.Option) error {
 	switch c.Use {
 	case "Source":
 		// TODO  report = runner.ScanSource()
-		re, err = runner.ScanFilesystem(context.Background(), opt.Option)
+		re, err = runner.ScanFilesystem(context.Background(), scanOption)
 		if err != nil {
 			return err
 		}
@@ -50,20 +47,19 @@ func runScan(c *cobra.Command, args []string, opt o.Option) error {
 		// TODO  report = runner.ScanRPMs()
 		return nil
 	case "Dockerfile":
-		re, err = runner.ScanFilesystem(context.Background(), opt.Option)
+		re, err = runner.ScanFilesystem(context.Background(), scanOption)
 		if err != nil {
 			return err
 		}
 	}
-	re, err = runner.Filter(context.Background(), opt.Option, report.Report)
+	re, err = runner.Filter(context.Background(), scanOption, report.Report)
 	if err != nil {
 		return err
 	}
-	if err = runner.Report(opt.Option, re); err != nil {
+	if err = runner.Report(scanOption, re); err != nil {
 		return err
 	}
 	return nil
-
 }
 
 func newCmdScan() *cobra.Command {
@@ -89,9 +85,8 @@ func newCmdScan() *cobra.Command {
 func newSubCmdSource() *cobra.Command {
 	var option o.Option
 	cmd := &cobra.Command{
-		Use:     "Source",
-		Aliases: []string{"-S"},
-		Short:   "Run this command in order to scan Source ...",
+		Use:   "Source",
+		Short: "Run this command in order to scan Source ...",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runScan(cmd, args, option)
 		},
@@ -111,13 +106,24 @@ func newSubCmdRPMs() *cobra.Command {
 func newSubCmdDokcerfile() *cobra.Command {
 	var option o.Option
 	cmd := &cobra.Command{
-		Use:     "Dockerfile",
-		Aliases: []string{"-D"},
-		Short:   "Run this command in order to scan dockerfile ...",
+		Use:   "Dockerfile",
+		Short: "Run this command in order to scan dockerfile ...",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runScan(cmd, args, option)
 		},
 		Args: cobra.MinimumNArgs(1),
 	}
+	flag := cmd.Flags()
+	flag.StringArrayVar(&option.PolicyNamespaces, "namespaces", []string{"users"}, "Rego namespaces")
+	flag.StringVar(&option.CacheDir, "cache-dir", defaultCacheDir(), "cache directory")
+	flag.StringVar(&option.Format, "format", "table", "report format table")
 	return cmd
+}
+
+func defaultCacheDir() string {
+	tmpDir, err := os.UserCacheDir()
+	if err != nil {
+		tmpDir = os.TempDir()
+	}
+	return filepath.Join(tmpDir, "ktib")
 }
