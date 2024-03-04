88e6963ab91532319d696e25cc795f40cc94976f
huayumeng <huayumeng@kylinos.cn>
2024-03-04 17:30:38 +0800

    Initialize the ktib builders add function

--- cmd/ktib/app/builders/add.go
+++ cmd/ktib/app/builders/add.go
@@ -12,8 +12,12 @@
 package builders
 
 import (
+	"fmt"
 	"gitee.com/openeuler/ktib/pkg/options"
 	"github.com/spf13/cobra"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 func ADDCmd() *cobra.Command {
@@ -22,12 +26,45 @@ func ADDCmd() *cobra.Command {
 		Use:   "add",
 		Short: "...",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return add(cmd, args, op)
+			source := args[0]
+			destination := args[1]
+			return add(cmd, source, destination, op)
 		},
 	}
 	return cmd
 }
 
-func add(cmd *cobra.Command, args []string, op options.BuildersOption) error {
+func add(cmd *cobra.Command, source string, destination string, op options.BuildersOption) error {
+	// 检查源文件是否存在
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		return fmt.Errorf("source file '%s' does not exist", source)
+	}
+
+	// 创建目标目录（如果不存在）
+	if err := os.MkdirAll(filepath.Dir(destination), 0755); err != nil {
+		return err
+	}
+
+	// 打开源文件
+	srcFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	// 创建目标文件
+	destFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	// 复制文件内容
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("File '%s' added to '%s'\n", source, destination)
 	return nil
 }
